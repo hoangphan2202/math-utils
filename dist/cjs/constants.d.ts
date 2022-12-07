e6fd466b5af99ddd621912c8f7236382188dc851
hoangphan <vanhoang22021997@gmail.com>
2022-12-07 15:47:53 +0700

    fix decimal

--- dist/cjs/constants.d.ts
+++ dist/cjs/constants.d.ts
@@ -1,5 +1,5 @@
 export declare const SECONDS_PER_YEAR: import("bignumber.js").default;
-export declare const USD_DECIMALS = 18;
+export declare const USD_DECIMALS = 8;
 export declare const WEI_DECIMALS = 18;
 export declare const RAY_DECIMALS = 27;
 export declare const LTV_PRECISION = 4;
